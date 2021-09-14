3ca4a36a9dee36dfaeac17c0eb411a6d65667759
Rajasekhar G <ggraj.pec@gmail.com>
2021-09-14 16:58:55 +0530

    Mail Task one more procedure added

--- quizserver/src/main/java/com/ab/quiz/helper/SendMailTask.java
+++ quizserver/src/main/java/com/ab/quiz/helper/SendMailTask.java
@@ -1,11 +1,15 @@
 package com.ab.quiz.helper;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.ab.quiz.pojo.Mail;
 import com.ab.quiz.services.MailService;
 import com.ab.quiz.services.MailServiceImpl;
 
 public class SendMailTask implements Runnable {
 	private Mail mail;
+	private static final Logger logger = LogManager.getLogger(SendMailTask.class);
 	
 	public SendMailTask(Mail mail) {
 		this.mail = mail;
@@ -13,8 +17,8 @@ public class SendMailTask implements Runnable {
 	
 	@Override
 	public void run() {
+		logger.info("This is in Mail Task {}", mail);
 		MailService mailService = new MailServiceImpl();
 		mailService.sendEmail(mail);
 	}
-
 }
